c6b876ff26efc03a61b81059b20406e567d6a181
Yash <yash.2024@nst.rishihood.edu.in>
2025-12-04 10:45:54 +0530

    Update index.js

--- Easy 1/index.js
+++ Easy 1/index.js
@@ -1,5 +1,9 @@
 const fs = require("fs");
+const path = require("path");
 
 function writeDataFile() {
-
+  const filePath = path.join(__dirname, "file.txt");
+  const text = "We will not miss the exam next time";
+  
+  fs.writeFileSync(filePath, text, "utf8")
 }
